665dc089162e89ece08eb4dac939156794944403
Kenya <kenya0324@gmail.com>
2020-02-15 07:54:17 +0000

    conflict modify

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,26 @@
+class PostsController < ApplicationController
+
+	def index
+
+	end
+
+	def show
+
+	end
+
+	def new
+		@post = Post.new
+
+	end
+
+	def create
+
+	end
+
+	private
+
+	def post_params
+        params.require(:post).permit(:user_id, category_ids: [], :post_content)
+    end
+
+end
