2710a26568332aa1d13939d32d3ef2c6a3be773b
Kenya <kenya0324@gmail.com>
2020-02-16 04:05:53 +0000

    Finish comment function

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,26 @@
 class CommentsController < ApplicationController
+
+	def create
+        @post = Post.find(params[:post_id])
+        @comment = @post.comments.build(comment_params)
+        @comment.user_id = current_user.id
+        if @comment.save
+           render :comment_create
+        else
+           render 'posts/show'
+        end
+    end
+
+    def destroy
+        @comment = Comment.find(params[:id])
+        if @comment.destroy
+           render :comment_create
+        end
+    end
+
+    private
+
+    def comment_params
+        params.require(:comment).permit(:content, :post_id, :user_id)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   root 'posts#index'
 
   resources :posts do
-    resources :comments, only: [:create]
+    resources :comments, only: [:create, :destroy]
   end
 
 end
